43a6420b13f489f6fc5cd68ae196986e80d45c2a
志田守 <>
2018-10-05 17:46:03 +0900

    rails4 finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def new
+    binding.pry
+    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def create
   end
 end
